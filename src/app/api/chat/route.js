f6ccf09e157745a43600655ced316dbf15937d56
Jeffrey Wang <ngjaywa@gmail.com>
2023-09-05 21:47:38 -0700

    switch to using openai for now

--- src/app/api/chat/route.js
+++ src/app/api/chat/route.js
@@ -2,7 +2,8 @@ import OpenAI from "openai";
 import { OpenAIStream, StreamingTextResponse } from "ai";
 
 const llamaCpp = new OpenAI({
-  baseURL: "http://localhost:8000/v1",
+  // baseURL: "http://localhost:8000/v1",
+  apiKey: process.env.OPENAI_API_KEY,
 });
 
 export const runtime = "edge";
@@ -10,6 +11,7 @@ export const runtime = "edge";
 export async function POST(req) {
   const { messages } = await req.json();
   const response = await llamaCpp.chat.completions.create({
+    model: "gpt-3.5-turbo",
     stream: true,
     max_tokens: 1000,
     messages,
