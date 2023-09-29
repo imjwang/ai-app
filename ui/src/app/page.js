12f4b45569a1f9891a02f5598f9d64ea8548bee6
Jeffrey Wang <ngjaywa@gmail.com>
2023-09-28 20:38:45 -0700

    remove js

--- ui/src/app/page.js
+++
@@ -1,9 +0,0 @@
-import Chat from "@/components/chat";
-
-export default function Home() {
-  return (
-    <>
-      <Chat className="p-2 lg:pb-12 lg:px-64 md:px-48 md:pb-8 sm:px-32 sm:pb-6" />
-    </>
-  );
-}
--- ui/src/app/upload/page.js
+++
@@ -1,8 +0,0 @@
-import FileInput from "@/components/fileinput";
-export default function Page() {
-  return (
-    <div className="flex p-12 h-full w-full justify-center">
-      <FileInput />
-    </div>
-  );
-}
